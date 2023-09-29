53d353696f8b9465284f8fa1e21e9ea810627b0d
Selim B <selim.benaissa@edu.esiee-it.fr>
2023-09-29 16:17:57 +0200

    Ajout de l'étoile pour pour ajout liste

--- src/MealCraft/static/js/liste.js
+++ src/MealCraft/static/js/liste.js
@@ -53,14 +53,27 @@ function search() {
 
             var addButton = document.createElement("button");
             addButton.className = 'ajouter-button';
-            addButton.textContent = 'Ajouter';
+            
+            // Créez un élément <i> pour l'icône de l'étoile de Font Awesome
+            var starIcon = document.createElement("i");
+            starIcon.className = 'fa-regular fa-star'; // Assurez-vous que la classe est correcte
+
+            addButton.appendChild(starIcon); // Ajoutez l'icône de l'étoile à addButton
+
             addButton.addEventListener('click', function () {
+                // Code pour basculer entre les classes CSS
+                if (starIcon.className === 'fa-regular fa-star') {
+                    starIcon.className = 'fa-solid fa-star';
+                } else {
+                    starIcon.className = 'fa-regular fa-star';
+                }
+
                 // Ajoutez ici le code pour gérer l'ajout de l'aliment
                 // Vous pouvez utiliser food.name, food.image ou d'autres données de food
                 alert('Vous avez ajouté : ' + food.name);
             });
-            listItem.appendChild(addButton);
 
+            listItem.appendChild(addButton);
             resultList.appendChild(listItem);
         });
         resultContainer.appendChild(resultList);
@@ -68,3 +81,4 @@ function search() {
         resultContainer.textContent = "Aucun résultat trouvé.";
     }
 }
+
